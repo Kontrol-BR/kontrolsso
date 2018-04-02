3b187fc1cbd9d850ab1477fda797974372fabab8
Kontrol-BR <guzzy@uol.com.br>
2018-04-02 14:10:25 -0300

    UPDATE - removed several lines

--- kontrolsso.sh
+++ kontrolsso.sh
@@ -38,10 +38,11 @@ kontrolsso: {
 EOF
 
 /usr/sbin/pkg update -r kontrolsso
-/usr/sbin/pkg install -r kontrolsso net/samba44 2> /dev/null
+#/usr/sbin/pkg install -r kontrolsso net/samba44 2> /dev/null
+/usr/sbin/pkg install -y -r kontrolsso net/samba44
 
 /usr/sbin/pkg unlock pkg
-/usr/sbin/pkg unlock Kontrol-2.4.1
+/usr/sbin/pkg unlock Kontrol-2.4.3
 
 /usr/sbin/pkg update
 
@@ -49,53 +50,53 @@ mkdir -p /var/db/samba4/winbindd_privileged
 chown -R :proxy /var/db/samba4/winbindd_privileged
 chmod -R 0750 /var/db/samba4/winbindd_privileged
 
-fetch -o /usr/local/pkg -q https://raw.githubusercontent.com/kontrolsecurity/kontrolsso/2.4.3-SAMBA4/samba.inc
-fetch -o /usr/local/pkg -q https://raw.githubusercontent.com/kontrolsecurity/kontrolsso/2.4.3-SAMBA4/samba.xml
+#fetch -o /usr/local/pkg -q https://raw.githubusercontent.com/kontrolsecurity/kontrolsso/2.4.3-SAMBA4/samba.inc
+#fetch -o /usr/local/pkg -q https://raw.githubusercontent.com/kontrolsecurity/kontrolsso/2.4.3-SAMBA4/samba.xml
 
-/usr/local/sbin/pfSsh.php <<EOF
-\$samba = false;
-foreach (\$config['installedpackages']['service'] as \$item) {
-  if ('samba' == \$item['name']) {
-    \$samba = true;
-    break;
-  }
-}
-if (\$samba == false) {
-	\$config['installedpackages']['service'][] = array(
-	  'name' => 'samba',
-	  'rcfile' => 'samba.sh',
-	  'executable' => 'smbd',
-	  'description' => 'Samba daemon'
- );
-}
-\$samba = false;
-foreach (\$config['installedpackages']['menu'] as \$item) {
-  if ('Samba (AD)' == \$item['name']) {
-    \$samba = true;
-    break;
-  }
-}
+#/usr/local/sbin/pfSsh.php <<EOF
+#\$samba = false;
+#foreach (\$config['installedpackages']['service'] as \$item) {
+#  if ('samba' == \$item['name']) {
+#    \$samba = true;
+#    break;
+#  }
+#}
+#if (\$samba == false) {
+#	\$config['installedpackages']['service'][] = array(
+#	  'name' => 'samba',
+#	  'rcfile' => 'samba.sh',
+#	  'executable' => 'smbd',
+#	  'description' => 'Samba daemon'
+# );
+#}
+#\$samba = false;
+#foreach (\$config['installedpackages']['menu'] as \$item) {
+#  if ('Samba (AD)' == \$item['name']) {
+#    \$samba = true;
+#    break;
+#  }
+#}
 
-if (\$samba == false) {
-  \$config['installedpackages']['menu'][] = array(
-    'name' => 'Samba (AD)',
-    'section' => 'Services',
-    'url' => '/pkg_edit.php?xml=samba.xml'
-  );
-}
-write_config();
-exec;
-exit
-EOF
+#if (\$samba == false) {
+#  \$config['installedpackages']['menu'][] = array(
+#    'name' => 'Samba (AD)',
+#    'section' => 'Services',
+#    'url' => '/pkg_edit.php?xml=samba.xml'
+#  );
+#}
+#write_config();
+#exec;
+#exit
+#EOF
 
 
-if [ ! "$(/usr/sbin/pkg info | grep Kontrol-pkg-squid)" ]; then
-	/usr/sbin/pkg install -r Kontrol Kontrol-pkg-squid
-fi
+#if [ ! "$(/usr/sbin/pkg info | grep Kontrol-pkg-squid)" ]; then
+#	/usr/sbin/pkg install -r Kontrol Kontrol-pkg-squid
+#fi
 
-cd /usr/local/pkg
-fetch -o - -q https://raw.githubusercontent.com/kontrolsecurity/kontrolsso/2.4.3-SAMBA4/squid_winbind_auth.patch | patch -b -p0 -f
-fetch -o /usr/local/pkg -q https://raw.githubusercontent.com/kontrolsecurity/kontrolsso/2.4.3-SAMBA4/squid.inc
+#cd /usr/local/pkg
+#fetch -o - -q https://raw.githubusercontent.com/kontrolsecurity/kontrolsso/2.4.3-SAMBA4/squid_winbind_auth.patch | patch -b -p0 -f
+#fetch -o /usr/local/pkg -q https://raw.githubusercontent.com/kontrolsecurity/kontrolsso/2.4.3-SAMBA4/squid.inc
 
 #if [ ! -f "/usr/local/etc/smb4.conf" ]; then
 #	touch /usr/local/etc/smb4.conf
